b0c2c7e8b3e076affdb6eb2d28f07f2287c3c74c
PavelRahman404 <paveltauker@gmail.com>
2024-01-31 19:38:30 +0600

    ContentHistory module

---
+++ models/ContentHistory.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+
+const historySchema = new mongoose.Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref : "User"
+    },
+    content:{
+        type: String,
+        required: true,
+    },
+   
+
+},
+{
+    timestamps: true,
+}
+);
+
+const ContentHistory = mongoose.mongo('ContentHistory',userSchema);
+module.exports = ContentHistory;
