7fa5d68f1dbaad40a9b8a4b30df415114af2f5d3
PavelRahman404 <paveltauker@gmail.com>
2024-02-02 20:01:16 +0600

    adding user router to server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
-
+const usersRouter = require('./routes/usersRouter');
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+//Routes
+app.use("/api/v1/users",usersRouter);
+
 
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
